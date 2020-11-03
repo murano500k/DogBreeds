2a186717a965b0e4082d2d335a605cbb7de28282
Artem Radchenko <murano500k@gmail.com>
2020-11-03 17:49:41 +0200

    Zoomable image added Menu back button implemented

--- app/src/main/java/com/murano500k/dogbreeds/MainActivity.kt
+++ app/src/main/java/com/murano500k/dogbreeds/MainActivity.kt
@@ -1,8 +1,10 @@
  package com.murano500k.dogbreeds
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.annotation.VisibleForTesting
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NavUtils
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -12,7 +14,18 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
          supportActionBar?.setDisplayHomeAsUpEnabled(true)
+         supportActionBar?.setDisplayShowHomeEnabled(true);
+
+         setContentView(R.layout.activity_main)
+     }
+     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+         return when (item.getItemId()) {
+             android.R.id.home -> {
+                 onBackPressed()
+                 true
+             }
+             else -> super.onOptionsItemSelected(item)
+         }
      }
 }
--- app/src/main/java/com/murano500k/dogbreeds/fragment/BreedListFragment.kt
+++ app/src/main/java/com/murano500k/dogbreeds/fragment/BreedListFragment.kt
@@ -2,18 +2,17 @@ package com.murano500k.dogbreeds.fragment
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.VisibleForTesting
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import com.murano500k.dogbreeds.DogBreedAdapter
 import com.murano500k.dogbreeds.R
 import com.murano500k.dogbreeds.TAG
-import com.murano500k.dogbreeds.databinding.ActivityMainBinding
 import com.murano500k.dogbreeds.databinding.FragmentListBinding
 import com.murano500k.dogbreeds.viewmodel.ListBreedsViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -26,21 +25,26 @@ class BreedListFragment : Fragment() {
     @VisibleForTesting
     private val breedsViewModel: ListBreedsViewModel by viewModels()
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        Log.v(TAG, "onCreateView: " )
-        val binding: FragmentListBinding = DataBindingUtil.inflate(inflater,
-            R.layout.fragment_list, container, false)
+        Log.v(TAG, "onCreateView: ")
+        val binding: FragmentListBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_list, container, false
+        )
         binding.vm = breedsViewModel
         binding.adapter = DogBreedAdapter()
         breedsViewModel.listBreedsLiveData.observe(viewLifecycleOwner, Observer {
             val adapter = DogBreedAdapter()
             adapter.addDogBreedList(it)
-            binding.adapter=adapter
+            binding.adapter = adapter
         })
         return binding.root
     }
-
 }
