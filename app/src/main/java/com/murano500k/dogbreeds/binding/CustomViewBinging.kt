37f5437b8bc2ea746ac3cbbc2da1959504a78224
Artem Radchenko <murano500k@gmail.com>
2020-11-04 18:17:52 +0200

    Revert "Removed unused code"
    This reverts commit 02819434c92d8367360951a7915e59fb0164418b.

--- app/src/main/java/com/murano500k/dogbreeds/binding/CustomViewBinging.kt
+++ app/src/main/java/com/murano500k/dogbreeds/binding/CustomViewBinging.kt
@@ -1,7 +1,6 @@
 package com.murano500k.dogbreeds.binding
 
 import android.annotation.SuppressLint
-import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.ProgressBar
@@ -13,7 +12,6 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.google.android.flexbox.*
 import com.murano500k.dogbreeds.R
-import com.murano500k.dogbreeds.gone
 import com.murano500k.dogbreeds.viewmodel.LoadingState
 
 
@@ -21,6 +19,8 @@ import com.murano500k.dogbreeds.viewmodel.LoadingState
 @BindingAdapter(value = ["setImageUrl"])
 fun ImageView.bindImageUrl(url: String?) {
     if (url != null && url.isNotBlank()) {
+
+
         Glide.with(context)
             .load(url)
             .apply(
@@ -32,15 +32,24 @@ fun ImageView.bindImageUrl(url: String?) {
     }
 }
 
-
 @BindingAdapter(value = ["setAdapter"])
 fun RecyclerView.bindRecyclerViewAdapter(adapter: RecyclerView.Adapter<*>) {
     this.run {
+        //this.setHasFixedSize(true)
+        /*this.layoutManager = FlexboxLayoutManager(context).apply {
+            flexWrap = FlexWrap.WRAP
+            flexDirection = FlexDirection.ROW
+            alignItems = AlignItems.STRETCH
+        }*/
         this.layoutManager = FlexboxLayoutManager(context).apply {
             flexWrap = FlexWrap.WRAP
             flexDirection = FlexDirection.ROW
+            //alignItems = AlignItems.STRETCH
             justifyContent = JustifyContent.SPACE_EVENLY
+
         }
+
+        //this.layoutManager = LinearLayoutManager(context)
         this.adapter = adapter
     }
 }
@@ -54,9 +63,4 @@ fun ProgressBar.progressVisibility(loadingState: LoadingState?) {
             LoadingState.Status.FAILED -> false
         }
     }
-}
-
-@BindingAdapter("gone")
-fun bindGone(view: View, shouldBeGone: Boolean) {
-    view.gone(shouldBeGone)
 }
